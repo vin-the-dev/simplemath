d320a3b12264771dd4b9ed59bd80b6617934eca0
Vineeth Vijayan <vinbhai4u@gmail.com>
2016-08-05 23:16:51 +0530

    Remove old

--- simplemath_old/Pods/Target Support Files/Pods-simplemath/Pods-simplemath-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_simplemath : NSObject
-@end
-@implementation PodsDummy_Pods_simplemath
-@end
--- simplemath_old/Pods/Target Support Files/Pods-simplemath/Pods-simplemath-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_simplemathVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_simplemathVersionString[];
-
